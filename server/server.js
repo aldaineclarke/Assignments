e6645fc50443928abd5e4a8a694b898ac311b29d
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-21 11:15:39 -0500

    updated the  server routes to have error handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
-const student = require("../server/models/student")
+const Student = require("../server/models/student")
 const cors = require("cors");
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({extended:true}));
 
 app.get("/api/students/", async (req, res, next)=>{
-    // res.json(student.find());
-    let students = await student.find();
-    res.status(200).json(students)
+    try{
+        let students = await Student.find();
+        res.status(200).json(students)
+
+    }catch(error){
+        res.status(400).json({message: "Cannot find students Collection"})
+    }
     
 });
 
@@ -21,31 +25,42 @@ app.post("/api/students/create",async(req, res, next)=>{
         form:req.body.form
 
     }
-    let newStudent = new student(studentData);
+    let newStudent = new Student(studentData);
     await newStudent.save()
     res.status(201).json(newStudent);
 });
 
 app.get("/api/students/:id",async(req,res,next)=>{
     let id = req.params.id;
-    let currentStudent = await student.findById(id);
-    res.status(200).json(currentStudent);
+    try{
+
+        let currentStudent = await Student.findById(id);
+        res.status(200).json(currentStudent);
+    }catch(err){
+        res.status(404).json({message: "Cannot get the student at id : "+ id })
+    }
 
 });
 
 app.delete("/api/students/:id",async(req,res,next)=>{
     let id = req.params.id;
-    await student.findByIdAndRemove(id);
-    res.status(200).json({message:"Deleted Successfully"})
+    try{
+        await Student.findByIdAndRemove(id);
+        res.status(200).json({message:"Deleted Successfully"})
+    }catch(err){
+        res.status(404).json({message: "Cannot delete the student with tid : "+ id })
+    }
 });
 
 app.patch("/api/students/:id", async(req,res, next)=>{
     let id = req.params.id;
-    let data = req.body;
-
-    let updatedStudent = await student.findByIdAndUpdate(id,data,{new: true});
-
-    res.status(200).json(updatedStudent);
+    try{
+        let data = req.body;
+        let updatedStudent = await student.findByIdAndUpdate(id,data,{new: true});
+        res.status(200).json(updatedStudent);
+    }catch(err){
+        res.status(404).json({message: "Cannot updated the student at id: "+ id })
+    }
 })
 
 app.listen(3000,()=>{
